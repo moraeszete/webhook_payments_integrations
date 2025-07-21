386b9afeda5cf5bd8748c891332aa483a1ffdc04
moraeszete <lucas.moraes24536@gmail.com>
2025-07-21 17:24:37 -0300

    Fiz: npm updates and uninstalling unused pack - making new hook template before fix the others

--- config/configServer.js
+++ config/configServer.js
@@ -10,8 +10,12 @@ module.exports = async () => {
   // MongoDB connection
   await mongodb.connect()
   global.mongo = mongodb.getDb()
+  console.log("MongoDB connected successfully")
   // redis connection
   await redisdb.connect()
   global.redis = redisdb.getDb()
+  console.log("Redis connected successfully")
+
   global.timestamps = timestamps
+  console.log("Timestamps utility initialized", global.timestamps.create())
 }
